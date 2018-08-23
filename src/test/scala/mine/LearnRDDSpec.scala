45bb10487c1cc04e0b86f775441c5bec3ab2dfd7
Felix Satyaputra <fsatyaputra@gmail.com>
2018-08-23 20:58:49 +1000

    Tests

--- src/test/scala/mine/LearnRDDSpec.scala
+++ src/test/scala/mine/LearnRDDSpec.scala
@@ -2,7 +2,6 @@ package mine
 
 import com.holdenkarau.spark.testing.SharedSparkContext
 import org.apache.spark.SparkContext
-import org.apache.spark.rdd.RDD
 import org.scalatest.{ FeatureSpec, GivenWhenThen, Matchers }
 
 class LearnRDDSpec extends FeatureSpec with GivenWhenThen with Matchers with SharedSparkContext {
@@ -51,6 +50,95 @@ class LearnRDDSpec extends FeatureSpec with GivenWhenThen with Matchers with Sha
         Person("Anna", 63))
       result.collect() shouldBe expectedResult
     }
+  }
+
+  feature("Person") {
+    scenario("list") {
+      When("the list of person is loaded")
+      val result = Person.list(FixturePath)
+
+      Then("the result should match the expected result")
+      val expectedResult = Seq(
+        Person("Thomas", 10),
+        Person("Janet", 3),
+        Person("Aaron", 12),
+        Person("Daniel", 45),
+        Person("Tim", 27),
+        Person("Nicole", 29),
+        Person("Anna", 63))
+      result.collect() shouldBe expectedResult
+    }
+
+    scenario("findByName") {
+      When("the person is found by name")
+      val result = Person.findByName(FixturePath, name = "Thomas")
+
+      Then("the result should match the expected result")
+      val expectedResult = Seq(Person("Thomas", 10))
+      result.collect() shouldBe expectedResult
+    }
+
+    scenario("findOlderThan") {
+      When("the person older than 30 is requested")
+      val result = Person.findOlderThan(FixturePath, age = 30)
+
+      Then("the result should match the expected result")
+      val expectedResult = Seq(
+        Person("Daniel", 45),
+        Person("Anna", 63))
+      result.collect() shouldBe expectedResult
+    }
+
+    scenario("sortByName") {
+      When("the list of person is sorted by name")
+      val result = Person.sortByName(FixturePath)
+
+      Then("the result should match the expected result")
+      val expectedResult = Seq(
+        Person("Aaron", 12),
+        Person("Anna", 63),
+        Person("Daniel", 45),
+        Person("Janet", 3),
+        Person("Nicole", 29),
+        Person("Thomas", 10),
+        Person("Tim", 27))
+      result.collect() shouldBe expectedResult
+    }
+
+    scenario("sortByAge") {
+      When("the list of person is sorted by age, youngest first")
+      val result = Person.sortByAge(FixturePath, oldestFirst = false)
+
+      Then("the result should match the expected result")
+      val expectedResult = Seq(
+        Person("Janet", 3),
+        Person("Thomas", 10),
+        Person("Aaron", 12),
+        Person("Tim", 27),
+        Person("Nicole", 29),
+        Person("Daniel", 45),
+        Person("Anna", 63))
+      result.collect() shouldBe expectedResult
+    }
+
+    scenario("oldest") {
+      When("the oldest person is requested")
+      val result = Person.oldest(FixturePath)
+
+      Then("the result should match the expected result")
+      result shouldBe Person("Anna", 63)
+    }
+
+    scenario("groupByFirstLetterOfTheName") {
+      When("the list of person is grouped by first letter of the Name")
+      val result = Person.groupByFirstLetterOfTheName(FixturePath)
+
+      Then("the result should match the expected result")
+      val expectedResult = Map(
+        "A" -> Seq(Person("Aaron", 12)))
+      result shouldBe expectedResult
+    }
+
   }
 
 }
